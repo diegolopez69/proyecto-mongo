7baec3d0b22857c3548412ae3500df46fe14d9bf
Diego López <diegolopezselva@hotmail.com>
2021-03-22 22:33:58 +0100

    The back-end is ready!! Fuck yeah!!

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Task = require('../models/Task');
 
-router.get('/', (req, res) => {
-    res.send('API TASK is goes here')
+router.get('/', async (req, res) => {
+    const task = await Task.find();
+    res.json(task);
 });
+
+router.post('/', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.json({
+        status: "Task Saved"
+    })
+});
+
+router.put('/:id', async (req, res) => {
+    await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.json({
+        status: "Task Updated"  
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id, req.body);
+    res.json({
+        status: "Task deleted"
+    })
+})
 
 module.exports = router;
